69a991de0f661b032e3199cdcc08ff32ec336d82
marakas73 <ysehgg@gmail.com>
2025-06-25 14:17:06 +0300

    Add console application run

--- src/main/java/org/marakas73/Main.java
+++ src/main/java/org/marakas73/Main.java
@@ -1,7 +1,24 @@
 package org.marakas73;
 
-public class Main {
+import org.marakas73.run.ConsoleFileScannerRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+    private final ConsoleFileScannerRunner consoleFileScannerRunner;
+
+    public Main(ConsoleFileScannerRunner consoleFileScannerRunner) {
+        this.consoleFileScannerRunner = consoleFileScannerRunner;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        consoleFileScannerRunner.run();
     }
 }
