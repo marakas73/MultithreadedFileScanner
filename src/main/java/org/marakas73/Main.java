2c40b1fbce3af26e223e40dd042ffb48feef72ce
marakas73 <ysehgg@gmail.com>
2025-06-25 16:42:47 +0300

    Remove console application running

--- src/main/java/org/marakas73/Main.java
+++ src/main/java/org/marakas73/Main.java
@@ -1,24 +1,11 @@
 package org.marakas73;
 
-import org.marakas73.run.ConsoleFileScannerRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main implements CommandLineRunner {
-    private final ConsoleFileScannerRunner consoleFileScannerRunner;
-
-    public Main(ConsoleFileScannerRunner consoleFileScannerRunner) {
-        this.consoleFileScannerRunner = consoleFileScannerRunner;
-    }
-
+public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        consoleFileScannerRunner.run();
     }
 }
