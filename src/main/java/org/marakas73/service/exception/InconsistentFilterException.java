29e8c53c34e59fc4a2c19f61cae1f5b5f7acd0e6
marakas73 <ysehgg@gmail.com>
2025-06-27 13:57:16 +0300

    Add exception for mismatching provided filter params

---
+++ src/main/java/org/marakas73/service/exception/InconsistentFilterException.java
@@ -0,0 +1,7 @@
+package org.marakas73.service.exception;
+
+public class InconsistentFilterException extends RuntimeException {
+    public InconsistentFilterException(String message) {
+        super(message);
+    }
+}
