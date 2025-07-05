397a4446a4d678de54b25b1bb272277e7687e924
marakas73 <ysehgg@gmail.com>
2025-07-05 21:57:06 +0300

    Make IntervalWrapper record class to ensure it serializable

--- src/main/java/org/marakas73/common/util/IntervalWrapper.java
+++ src/main/java/org/marakas73/common/util/IntervalWrapper.java
@@ -3,22 +3,12 @@ package org.marakas73.common.util;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
 
-public class IntervalWrapper<T> {
-    private final T start;
-    private final T end;
-
+public record IntervalWrapper<T>(T start, T end) {
     public IntervalWrapper(
             @JsonProperty("start") @Nullable T start,
-            @JsonProperty("end") @Nullable T end) {
+            @JsonProperty("end") @Nullable T end
+    ) {
         this.start = start;
         this.end = end;
-    }
-
-    public T getStart() {
-        return start;
-    }
-
-    public T getEnd() {
-        return end;
     }
 }
