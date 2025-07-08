45796f7c86da55228cf77cb698934cc2e5370275
marakas73 <ysehgg@gmail.com>
2025-07-08 18:32:54 +0300

    Refactor controller to return new response DTO instead of model

--- src/main/java/org/marakas73/controller/FileScannerHttpController.java
+++ src/main/java/org/marakas73/controller/FileScannerHttpController.java
@@ -1,10 +1,12 @@
 package org.marakas73.controller;
 
 import jakarta.validation.Valid;
+import org.marakas73.controller.dto.response.FileScanResponseDto;
 import org.marakas73.controller.dto.response.ResponseWrapper;
 import org.marakas73.controller.mapper.FileScanRequestMapper;
 import org.marakas73.controller.dto.request.FileScanRequestDto;
 import org.marakas73.controller.dto.response.ResponseStatus;
+import org.marakas73.controller.mapper.FileScanResultMapper;
 import org.marakas73.model.FileScanRequest;
 import org.marakas73.model.FileScanResult;
 import org.marakas73.service.filescanner.FileScanner;
@@ -19,21 +21,28 @@ import java.util.Map;
 public class FileScannerHttpController {
     private final FileScanner fileScanner;
     private final FileScanRequestMapper fileScanRequestMapper;
+    private final FileScanResultMapper fileScanResultMapper;
 
-    public FileScannerHttpController(FileScanner fileScanner, FileScanRequestMapper fileScanRequestMapper) {
+    public FileScannerHttpController(
+            FileScanner fileScanner,
+            FileScanRequestMapper fileScanRequestMapper,
+            FileScanResultMapper fileScanResultMapper
+    ) {
         this.fileScanner = fileScanner;
         this.fileScanRequestMapper = fileScanRequestMapper;
+        this.fileScanResultMapper = fileScanResultMapper;
     }
 
     @PostMapping
-    public ResponseEntity<ResponseWrapper<FileScanResult>> startScan(
+    public ResponseEntity<ResponseWrapper<FileScanResponseDto>> startScan(
             @Valid @RequestBody FileScanRequestDto requestDto
     ) {
         try{
             FileScanRequest scanRequest = fileScanRequestMapper.toModel(requestDto);
             FileScanResult scanResponse = fileScanner.startScan(scanRequest);
+            FileScanResponseDto responseDto = fileScanResultMapper.toResponseDto(scanResponse);
 
-            return ResponseEntity.ok().body(new ResponseWrapper<>(ResponseStatus.SUCCESS, Map.of(), scanResponse));
+            return ResponseEntity.ok().body(new ResponseWrapper<>(ResponseStatus.SUCCESS, Map.of(), responseDto));
         } catch (RuntimeException e) {
             Map<String, String> error = Map.of(
                     e.getClass().getSimpleName(),
